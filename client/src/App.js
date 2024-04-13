e888e2206bb3249e27de1ae279b4ad2f24a613d0
cramerj1470 <john@Cramerec.com>
2024-04-13 01:00:57 -0400

    client react app start

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './fxVE.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="sitename">CoolCat</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="mt20">
+          Where you get smaller and smaller while you get larger and larger
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
